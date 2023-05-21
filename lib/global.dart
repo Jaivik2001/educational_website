7e8ddc9c43ce21309e09d6d9afe801ac036acbc5
Jaivik <jaivikmoradiya2001@gmail.com>
2023-05-21 13:42:39 +0530

    Initial commit

---
+++ lib/global.dart
@@ -0,0 +1,52 @@
+class Val {
+  static List history = [];
+  static List bookmark = [];
+  static List<Map<String, dynamic>> website = [
+    {
+      'name': 'Google',
+      'img':
+      'https://news-cdn.softpedia.com/images/news2/the-new-google-logo-is-a-lesson-in-modern-design-490648-3.jpg',
+      'site': 'https://www.google.com/',
+    },
+    {
+      'name': 'Wikipedia',
+      'img':
+      'https://cdn-icons-png.flaticon.com/512/48/48927.png',
+      'site': 'https://www.wikipedia.org/',
+    },
+    {
+      'name': 'W3Schools',
+      'img':
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/W3Schools_logo.svg/512px-W3Schools_logo.svg.png?20210601004220',
+      'site': 'https://www.w3schools.com/',
+    },
+    {
+      'name': 'JavatPoint',
+      'img':
+      'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/v1455524861/bhsyyazdiwk6qiwnertz.png',
+      'site': 'https://www.javatpoint.com/',
+    },
+    {
+      'name': 'TutorialsPoint',
+      'img': 'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png',
+      'site': 'https://www.tutorialspoint.com/',
+    },
+    {
+      'name': 'Getepic',
+      'img':
+      'https://logos-world.net/wp-content/uploads/2022/04/Epic-Old-Logo.png',
+      'site': 'https://www.getepic.com/',
+    },
+    {
+      'name': 'Clever',
+      'img':
+      'https://filecabinet10.eschoolview.com/6800DEC1-6CCC-4333-A084-F17A2FD93129/31f44c10-a22a-4c0b-8613-21321897496b.png',
+      'site': 'https://clever.com/',
+    },{
+      'name': 'Stack Overflow',
+      'img':
+      'https://toppng.com/uploads/preview/stack-overflow-logo-11609377963rmch0kvpyf.png',
+      'site': 'https://stackoverflow.com/',
+    },
+  ];
+}
